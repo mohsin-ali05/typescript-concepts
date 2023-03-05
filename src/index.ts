759031cf147367ed306e1b324c248696b3155780
mohsin-ali05 <125805046+mohsin-ali05@users.noreply.github.com>
2023-03-05 14:44:01 +0500

    created a simple greet function

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// // Function Declaration
+// :void Is the return type
+// user_name is called parameter.
+
+function greet(user_name:string):void{
+   console.log("Welcome ",user_name)
+}
+// Function Call
+greet("Mohsin");
